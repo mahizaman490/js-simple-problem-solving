f6ec73135011195054043a18de11499ddf6f05cc
Mahi Zaman <mahizaman490@gmail.com>
2024-02-13 15:06:17 +0600

    added another way to set mode

--- index.js
+++ index.js
@@ -928,3 +928,39 @@
 //   }
 //   console.log(mode);
 // })
+
+
+
+// let btn = document.querySelector("#btn");
+// let body = document.querySelector("body");
+// let currentMode = "light";
+// btn.addEventListener("click",()=>{
+//   if(currentMode=="light"){
+//     currentMode = "dark";
+//     body.classList.add("dark");
+//     body.classList.remove("light");
+//   }else{
+//     currentMode = "light";
+//     body.classList.add("light");
+//     body.classList.remove("dark");
+
+//   }
+// })
+
+
+
+// let body = document.querySelector("body");
+// let btn = document.querySelector("#btn");
+// let mode = "light";
+// btn.addEventListener("click",()=>{
+// if(mode == "light"){
+//   mode = "dark";
+//   body.classList.add("dark");
+//   body.classList.remove("light");
+// }else{
+//   mode = "light";
+//   body.classList.add("light");
+//   body.classList.remove("dark");
+// }
+// console.log(mode);
+// })
